4c38868034170e82eeee12f649a66cd11ed7824f
RedMarkD <reinout.de.bleser@gmail.com>
2021-11-17 11:13:34 +0100

    layers and player added

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+window.localStorage
+
+//todo: NO MAGIC VALUES IN FUNCTIONS!
+//TODO: oop? create different classes for the different elements.
+
+
+
+window.addEventListener('keydown', function(event) {
+    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+    console.log(key);
+    move();
+});
+
+function move() {
+    on
+}
+
+
+
+
