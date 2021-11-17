701a4fa1c61a01c97495218cc51ce536aed3cda5
RedMarkD <reinout.de.bleser@gmail.com>
2021-11-17 14:58:23 +0100

    the world appears flat

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,14 @@ window.localStorage
 
 const moveArray = []
 
+const player = document.getElementById("player");
+const clouds = document.getElementById("clouds");
+const ground = document.getElementById("ground");
+const hills = document.getElementById("hills");
+const trees = document.getElementById("trees");
+let remember = 0;
+const funbod = document.body.style
+
 //setTimeout()
 
 document.addEventListener('keydown', function(event) {
@@ -27,29 +35,33 @@ document.addEventListener('keyup', function(event) {
     console.log(moveArray);
     changeChar()
 });
+console.log(remember)
 //backgroundposition Xright
 function changeChar(){
     if (moveArray.includes("ArrowRight")){
-        document.getElementById("player").style.backgroundImage="url(../img/run.gif)";
-        document.getElementById("player").style.transform = "scaleX(1)";
-        document.getElementById("layer4").style.backgroundPositionX += "1px";
+        player.style.backgroundImage="url(../img/run.gif)";
+        player.style.transform = "scaleX(1)";
+        remember -= 2;
+        moveBack()
     }
     else if (moveArray.includes("ArrowLeft")){
-        document.getElementById("player").style.transform = "scaleX(-1)";
-        document.getElementById("player").style.backgroundImage="url(../img/run.gif)";
+        player.style.transform = "scaleX(-1)";
+        player.style.backgroundImage="url(../img/run.gif)";
+        remember += 2;
+        moveBack()
     }
     else {
-        document.getElementById("player").style.backgroundImage="url(../img/idle.gif)"
+        player.style.backgroundImage="url(../img/idle.gif)"
     }
 }
 
 function moveBack(){
-    if (moveArray.includes("ArrowRight")){
-
-    }
-
+    clouds.style.backgroundPositionX = remember+"px";
+    hills.style.backgroundPositionX = 2*remember+"px";
+    trees.style.backgroundPositionX = 3*remember+"px";
+    ground.style.backgroundPositionX = 4*remember+"px";
+    document.body.style.setProperty("-webkit-transform", "rotate("+remember+"deg)", null);
 }
-
 
 
 
