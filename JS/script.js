63c0710760319c826703d660d23aa418b27d36a5
RedMarkD <reinout.de.bleser@gmail.com>
2021-11-18 11:33:55 +0100

    working spinning globe with parallax

--- JS/script.js
+++ JS/script.js
@@ -4,14 +4,18 @@ window.localStorage
 //TODO: oop? create different classes for the different elements.
 
 const moveArray = []
+const img = "img/02_trees_and_bushes.png";
 
 const player = document.getElementById("player");
 const clouds = document.getElementById("clouds");
 const ground = document.getElementById("ground");
 const hills = document.getElementById("hills");
 const trees = document.getElementById("trees");
+const roundground = document.getElementById("Roundground");
+const roundtrees = document.getElementById("Roundtrees");
+const roundhills = document.getElementById("Roundhills");
+const container = document.getElementById('container');
 let remember = 0;
-const funbod = document.body.style
 
 //setTimeout()
 
@@ -41,13 +45,13 @@ function changeChar(){
     if (moveArray.includes("ArrowRight")){
         player.style.backgroundImage="url(../img/run.gif)";
         player.style.transform = "scaleX(1)";
-        remember -= 2;
+        remember -= 1;
         moveBack()
     }
     else if (moveArray.includes("ArrowLeft")){
         player.style.transform = "scaleX(-1)";
         player.style.backgroundImage="url(../img/run.gif)";
-        remember += 2;
+        remember += 1;
         moveBack()
     }
     else {
@@ -56,14 +60,15 @@ function changeChar(){
 }
 
 function moveBack(){
-    clouds.style.backgroundPositionX = remember+"px";
-    hills.style.backgroundPositionX = 2*remember+"px";
-    trees.style.backgroundPositionX = 3*remember+"px";
-    ground.style.backgroundPositionX = 4*remember+"px";
-    document.body.style.setProperty("-webkit-transform", "rotate("+remember+"deg)", null);
+    // clouds.style.backgroundPositionX = remember+"px";
+    // hills.style.backgroundPositionX = 4*remember+"px";
+    // trees.style.backgroundPositionX = 6*remember+"px";
+    // ground.style.backgroundPositionX = 8*remember+"px";
+    // container.style.setProperty("-webkit-transform", "rotate("+remember+"deg)", null);
+    roundground.style.setProperty("-webkit-transform", "rotate("+1.5*remember+"deg)", null);
+    roundtrees.style.setProperty("-webkit-transform", "rotate("+remember+"deg)", null);
+    roundhills.style.setProperty("-webkit-transform", "rotate("+0.5*remember+"deg)", null);
+    //document.body.style.setProperty("-webkit-transform", "rotate("+-remember+"deg)", null);
 }
 
-
-
-
-
+//container and overflow hidden. for half circle turn::
