f9fab9e9d7fc48c7e425d4870e77fb5d4f19b38f
RedMarkD <reinout.de.bleser@gmail.com>
2021-11-19 14:50:14 +0100

    first part works great, second part gives a warping burger

--- JS/script.js
+++ JS/script.js
@@ -32,7 +32,6 @@ document.addEventListener('keydown', function(event) {
     // console.log(moveArray);
     changeChar()
 });
-moveBurger()
 document.addEventListener('keyup', function(event) {
     const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
     if (moveArray.includes(key)){
@@ -43,7 +42,6 @@ document.addEventListener('keyup', function(event) {
     //console.log(moveArray);
     changeChar()
 });
-console.log(remember)
 //backgroundposition Xright
 function changeChar(){
     if (moveArray.includes("ArrowUp")){
@@ -94,18 +92,38 @@ function moveBurger(){
     burger.style.left = 10*remember+"px";
     // burger.style.top =  (15*remember)+"px";
 }
-function collision() {
-    console.log(player.style.left);
-    console.log(burger.style.left);
-    if (burger.style.left >= 732.3+"px"){
-        console.log("HIT")
+
+function touch() {
+
+    let d1 = document.getElementById('player').getBoundingClientRect();
+    let d2 = document.getElementById('burger').getBoundingClientRect();
+
+    function touching(div1,div2){
+        let ox = Math.abs(d1.x - d2.x) < (d1.x < d2.x ? d2.width : d1.width);
+        let oy= Math.abs(d1.y - d2.y) < (d1.y < d2.y ? d2.height : d1.height);
+        return ox && oy;
     }
+
+    let t = touching(d1, d2);
+    console.log(t);
+    if (t === true)
+    {
+        let random = Math.ceil(Math.random() * 100);
+        console.log("HIT");
+        burger.style.left = random + "%";
+        t = false;
+    }
+    else
+    {
+        console.log("no hit / respawn");
+    }
+
 }
 
 function runAll(){
     moveBack();
     moveBurger();
-    collision()
+    touch();
 }
 
 //container and overflow hidden. for half circle turn::
